f36c412ce83853b112a187a3fa08a8fcd7cda886
Balasaiteja1 <balasaiteja17@gmail.com>
2024-03-27 13:25:28 +0530

    changing H1 font size in DOM file

--- Section-16 DOM/DOM Challenge Starting Files/index.js
+++ Section-16 DOM/DOM Challenge Starting Files/index.js
@@ -2,3 +2,5 @@ const { Button } = require("bootstrap");
 
 let heading = document.firstElementChild.lastElementChild.firstElementChild;
 heading.innerHTML = "bala";
+
+let sizeInc = document.firstElementChild.lastElementChild.firstElementChild.classList.toggle("huge");
