469d435e1ef3afeada69099ea39b81eb28364ab6
Balasaiteja <balasaiteja17@gmail.com>
2024-04-01 12:00:20 +0530

    adding white text after click in javaScript

--- Section-18/Drum Kit Starting Files/index.js
+++ Section-18/Drum Kit Starting Files/index.js
@@ -2,6 +2,6 @@ var total_drums = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < total_drums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked!");
+    console.log((this.style.color = "white"));
   });
 }
